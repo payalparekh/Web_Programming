744e74cb0bd36b2b2b7e85a49549c4ff71ec0d72
payalparekh <155072547+payalparekh@users.noreply.github.com>
2024-01-10 23:03:05 +0530

    Add files via upload

---
+++ print_demo.php
@@ -0,0 +1,9 @@
+<?php
+echo"<table border=1 bordercolor=blue>
+<tr><td>Sr.no</td>  <td>Email</td> <td>City</td></tr>
+<tr><td>1</td> <td>abc@gamil.com</td> <td>Rajkot</td></tr>
+
+
+
+</table>";
+?>
