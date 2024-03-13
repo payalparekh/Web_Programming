b2d7f5368ca38c0b3e2cd5f00e55da77ff64f6e5
payalparekh <155072547+payalparekh@users.noreply.github.com>
2024-03-13 23:43:54 +0530

    Add files via upload

---
+++ myadmin.php
@@ -0,0 +1,8 @@
+<html>
+    <form>
+        Enter about us<input type="text" name="about_me" /><br>
+        Enter skills<input type="my_skills" /><br>
+       <input type="submit" name="submit" />
+    </form>
+    </html>
+    
